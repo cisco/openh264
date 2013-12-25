39b6b735a0c59fd118ebaabfb6d5ca40edeea798
Licai Guo <guolicai@gmail.com>
2013-12-25 08:48:46 +0800

    fix mac build file, asm still not work

--- build/platform-darwin.mk
+++ build/platform-darwin.mk
@@ -1,9 +1,9 @@
-#USE_ASM = No  # We don't have ASM working on Mac yet
+USE_ASM = No  # We don't have ASM working on Mac yet
 ASM = nasm
 CFLAGS +=  -fPIC
 LDFLAGS += -lpthread
 ifeq ($(ENABLE64BIT), Yes)
-ASMFLAGS += -f macho32 --prefix _
+ASMFLAGS += -f macho64 --prefix _
 else
-ASMFLAGS += -f macho64 
+ASMFLAGS += -f macho32 
 endif
