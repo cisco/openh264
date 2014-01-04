8a7a15ff30c3b420f2a1b7a84510f9896b18239f
volvet <qizh@cisco.com>
2014-01-04 08:20:56 +0800

    fix merge conflict

--- build/platform-darwin.mk
+++ build/platform-darwin.mk
@@ -2,11 +2,7 @@
 ASM = nasm
 CFLAGS += -Werror -fPIC
 LDFLAGS += -lpthread
-<<<<<<< HEAD
-ASMFLAGS += --prefix _
-=======
 ASMFLAGS += --prefix _ -DNOPREFIX
->>>>>>> 64bits_Support
 ifeq ($(ENABLE64BIT), Yes)
 ASMFLAGS += -f macho64
 else
--- build/platform-linux.mk
+++ build/platform-linux.mk
@@ -1,10 +1,7 @@
 ASM = nasm
 CFLAGS += -Werror -fPIC -DLINUX -D__NO_CTYPE
 LDFLAGS += -lpthread
-<<<<<<< HEAD
-=======
 ASMFLAGS += -DNOPREFIX
->>>>>>> 64bits_Support
 ifeq ($(ENABLE64BIT), Yes)
 ASMFLAGS += -f elf64
 else 
