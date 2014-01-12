6082566f4cefcfdf0d2ac9ab8e2ddebfa0f6c63a
Varun B Patil <varun.basavaraj.patil@gmail.com>
2014-01-12 18:22:31 +0530

    Single copy of yuv test streams.

--- testbin/CmdLineExample.sh
+++ testbin/CmdLineExample.sh
@@ -2,6 +2,6 @@
 ../h264enc welsenc_vd_rc.cfg
 ../h264enc welsenc_arbitrary_res.cfg
 
-../h264dec test_vd_1d.264 test_vd_1d.yuv
-../h264dec test_vd_rc.264 test_vd_rc.yuv
+../h264dec ../res/test_vd_1d.264 test_vd_1d.yuv
+../h264dec ../res/test_vd_rc.264 test_vd_rc.yuv
 ../h264dec test.264 test.yuv
