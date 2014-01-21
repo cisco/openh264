043575ffb0309be318776673551e0abfa3089c55
Martin Storsjö <martin@martin.st>
2014-01-21 16:12:57 +0200

    Add a public C API to the library
    Add a struct that matches the C++ interface vtable.
    
    This requires that the C++ interface methods are declared to use
    the same calling convention as normal C functions, and that the
    C struct exactly matches the layout and ordering of the C++
    virtual table - MSVC seemed to reorder methods if there were
    overloaded methods.

--- codec/decoder/plus/inc/welsDecoderExt.h
+++ codec/decoder/plus/inc/welsDecoderExt.h
@@ -59,8 +59,8 @@ class CWelsDecoder : public ISVCDecoder {
 CWelsDecoder (void_t);
 virtual ~CWelsDecoder();
 
-virtual long Initialize (void_t* pParam, const INIT_TYPE keInitType);
-virtual long Uninitialize();
+virtual long EXTAPI Initialize (void_t* pParam, const INIT_TYPE keInitType);
+virtual long EXTAPI Uninitialize();
 
 /***************************************************************************
 *	Description:
@@ -74,28 +74,28 @@ virtual long Uninitialize();
 *
 *	return: if decode frame success return 0, otherwise corresponding error returned.
 ***************************************************************************/
-virtual DECODING_STATE DecodeFrame (const unsigned char* kpSrc,
-                                    const int kiSrcLen,
-                                    unsigned char** ppDst,
-                                    int* pStride,
-                                    int& iWidth,
-                                    int& iHeight);
+virtual DECODING_STATE EXTAPI DecodeFrame (const unsigned char* kpSrc,
+                                           const int kiSrcLen,
+                                           unsigned char** ppDst,
+                                           int* pStride,
+                                           int& iWidth,
+                                           int& iHeight);
 
-virtual DECODING_STATE DecodeFrame2 (const unsigned char* kpSrc,
-                                    const int kiSrcLen,
-                                    void_t** ppDst,
-                                    SBufferInfo* pDstInfo);
-virtual DECODING_STATE DecodeFrameEx (const unsigned char* kpSrc,
-                                      const int kiSrcLen,
-                                      unsigned char* pDst,
-                                      int iDstStride,
-                                      int& iDstLen,
-                                      int& iWidth,
-                                      int& iHeight,
-                                      int& color_format);
+virtual DECODING_STATE EXTAPI DecodeFrame2 (const unsigned char* kpSrc,
+                                            const int kiSrcLen,
+                                            void_t** ppDst,
+                                            SBufferInfo* pDstInfo);
+virtual DECODING_STATE EXTAPI DecodeFrameEx (const unsigned char* kpSrc,
+                                             const int kiSrcLen,
+                                             unsigned char* pDst,
+                                             int iDstStride,
+                                             int& iDstLen,
+                                             int& iWidth,
+                                             int& iHeight,
+                                             int& color_format);
 
-virtual long SetOption (DECODER_OPTION eOptID, void_t* pOption);
-virtual long GetOption (DECODER_OPTION eOptID, void_t* pOption);
+virtual long EXTAPI SetOption (DECODER_OPTION eOptID, void_t* pOption);
+virtual long EXTAPI GetOption (DECODER_OPTION eOptID, void_t* pOption);
 
  private:
 PWelsDecoderContext 				m_pDecContext;
