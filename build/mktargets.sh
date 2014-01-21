35ad8cb3a5ab5c0172e5e30227518ab593421352
Martin Storsjö <martin@martin.st>
2014-01-21 14:51:19 +0200

    Remove an exclude of an nonexistent file

--- build/mktargets.sh
+++ build/mktargets.sh
@@ -5,6 +5,6 @@
 (cd codec/processing && python ../../build/mktargets.py --directory codec/processing --library processing --exclude wels_process.cpp --exclude WelsVideoProcessor.cpp)
 
 (cd codec/console/dec && python ../../../build/mktargets.py --directory codec/console/dec --binary h264dec)
-(cd codec/console/enc && python ../../../build/mktargets.py --directory codec/console/enc --binary h264enc --exclude enc_console.h)
+(cd codec/console/enc && python ../../../build/mktargets.py --directory codec/console/enc --binary h264enc)
 (cd test && python ../build/mktargets.py --directory test --binary codec_unittest)
 (cd gtest && python ../build/mktargets.py --directory gtest --library gtest --out ../build/gtest-targets.mk --cpp-suffix .cc --include gtest-all.cc)
