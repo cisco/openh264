9a875532fd3f96cfbeb41cf40b3a8359d3d8e679
Licai Guo <guolicai@gmail.com>
2014-01-22 18:35:50 -0800

    add level limits

--- codec/decoder/core/inc/parameter_sets.h
+++ codec/decoder/core/inc/parameter_sets.h
@@ -39,6 +39,18 @@
 
 namespace WelsDec {
 
+typedef struct TagLevelLimits {
+  int32_t iMaxMBPS; // Max macroblock processing rate(MB/s)
+  int32_t iMaxFS;   // Max frame sizea(MBs)
+  int32_t iMaxDPBMbs;// Max decoded picture buffer size(MBs)
+  int32_t iMaxBR; // Max video bit rate
+  int32_t iMaxCPB; // Max CPB size
+  int16_t iMinVmv; // Vertical MV component range upper bound
+  int16_t iMaxVmv; // Vertical MV component range lower bound
+  int16_t iMinCR;  // Min compression ration
+  int16_t iMaxMvsPer2Mb; // Max number of motion vectors per two consecutive MBs
+} SLevelLimits;
+
 /* Sequence Parameter Set, refer to Page 57 in JVT X201wcm */
 typedef struct TagSps {
 int32_t	    iSpsId;
@@ -87,6 +99,7 @@ bool_t		bSeparateColorPlaneFlag;
 bool_t		bQpPrimeYZeroTransfBypassFlag;
 bool_t		bSeqScalingMatrixPresentFlag;
 bool_t		bSeqScalingListPresentFlag[12];
+const SLevelLimits *pSLevelLimits;
 } SSps, *PSps;
 
 
