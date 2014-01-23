8a932fbe8567024483bed725c724c0fe980f1fb1
Martin Storsjö <martin@martin.st>
2014-01-23 18:53:57 +0200

    Remove excludes of nonexistent files

--- build/mktargets.sh
+++ build/mktargets.sh
@@ -2,7 +2,7 @@
 (cd codec/decoder && python ../../build/mktargets.py --directory codec/decoder --library decoder --exclude StdAfx.cpp)
 (cd codec/encoder && python ../../build/mktargets.py --directory codec/encoder --library encoder --exclude DllEntry.cpp)
 (cd codec/common && python ../../build/mktargets.py --directory codec/common --library common)
-(cd codec/processing && python ../../build/mktargets.py --directory codec/processing --library processing --exclude wels_process.cpp --exclude WelsVideoProcessor.cpp)
+(cd codec/processing && python ../../build/mktargets.py --directory codec/processing --library processing)
 
 (cd codec/console/dec && python ../../../build/mktargets.py --directory codec/console/dec --binary h264dec)
 (cd codec/console/enc && python ../../../build/mktargets.py --directory codec/console/enc --binary h264enc)
