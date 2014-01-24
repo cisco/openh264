d0185ab2537e4e43bbc2d71c222fd99fdd383d03
Martin Storsjö <martin@martin.st>
2014-01-24 14:35:38 +0200

    Remove redundant /./ parts from pathnames in makefiles
    This is mostly a cosmetic improvement for the quiet make output.

--- codec/common/targets.mk
+++ codec/common/targets.mk
@@ -1,23 +1,23 @@
 COMMON_SRCDIR=codec/common
 COMMON_CPP_SRCS=\
-	$(COMMON_SRCDIR)/./cpu.cpp\
-	$(COMMON_SRCDIR)/./crt_util_safe_x.cpp\
-	$(COMMON_SRCDIR)/./deblocking_common.cpp\
-	$(COMMON_SRCDIR)/./logging.cpp\
-	$(COMMON_SRCDIR)/./WelsThreadLib.cpp\
+	$(COMMON_SRCDIR)/cpu.cpp\
+	$(COMMON_SRCDIR)/crt_util_safe_x.cpp\
+	$(COMMON_SRCDIR)/deblocking_common.cpp\
+	$(COMMON_SRCDIR)/logging.cpp\
+	$(COMMON_SRCDIR)/WelsThreadLib.cpp\
 
 COMMON_OBJS += $(COMMON_CPP_SRCS:.cpp=.o)
 ifeq ($(USE_ASM), Yes)
 COMMON_ASM_SRCS=\
-	$(COMMON_SRCDIR)/./asm_inc.asm\
-	$(COMMON_SRCDIR)/./cpuid.asm\
-	$(COMMON_SRCDIR)/./deblock.asm\
-	$(COMMON_SRCDIR)/./expand_picture.asm\
-	$(COMMON_SRCDIR)/./mb_copy.asm\
-	$(COMMON_SRCDIR)/./mc_chroma.asm\
-	$(COMMON_SRCDIR)/./mc_luma.asm\
-	$(COMMON_SRCDIR)/./satd_sad.asm\
-	$(COMMON_SRCDIR)/./vaa.asm\
+	$(COMMON_SRCDIR)/asm_inc.asm\
+	$(COMMON_SRCDIR)/cpuid.asm\
+	$(COMMON_SRCDIR)/deblock.asm\
+	$(COMMON_SRCDIR)/expand_picture.asm\
+	$(COMMON_SRCDIR)/mb_copy.asm\
+	$(COMMON_SRCDIR)/mc_chroma.asm\
+	$(COMMON_SRCDIR)/mc_luma.asm\
+	$(COMMON_SRCDIR)/satd_sad.asm\
+	$(COMMON_SRCDIR)/vaa.asm\
 
 COMMON_OBJS += $(COMMON_ASM_SRCS:.asm=.o)
 endif
--- codec/processing/targets.mk
+++ codec/processing/targets.mk
@@ -1,29 +1,29 @@
 PROCESSING_SRCDIR=codec/processing
 PROCESSING_CPP_SRCS=\
-	$(PROCESSING_SRCDIR)/./src/adaptivequantization/AdaptiveQuantization.cpp\
-	$(PROCESSING_SRCDIR)/./src/backgrounddetection/BackgroundDetection.cpp\
-	$(PROCESSING_SRCDIR)/./src/common/memory.cpp\
-	$(PROCESSING_SRCDIR)/./src/common/thread.cpp\
-	$(PROCESSING_SRCDIR)/./src/common/WelsFrameWork.cpp\
-	$(PROCESSING_SRCDIR)/./src/common/WelsFrameWorkEx.cpp\
-	$(PROCESSING_SRCDIR)/./src/complexityanalysis/ComplexityAnalysis.cpp\
-	$(PROCESSING_SRCDIR)/./src/denoise/denoise.cpp\
-	$(PROCESSING_SRCDIR)/./src/denoise/denoise_filter.cpp\
-	$(PROCESSING_SRCDIR)/./src/downsample/downsample.cpp\
-	$(PROCESSING_SRCDIR)/./src/downsample/downsamplefuncs.cpp\
-	$(PROCESSING_SRCDIR)/./src/imagerotate/imagerotate.cpp\
-	$(PROCESSING_SRCDIR)/./src/imagerotate/imagerotatefuncs.cpp\
-	$(PROCESSING_SRCDIR)/./src/scenechangedetection/SceneChangeDetection.cpp\
-	$(PROCESSING_SRCDIR)/./src/scenechangedetection/SceneChangeDetectionCommon.cpp\
-	$(PROCESSING_SRCDIR)/./src/vaacalc/vaacalcfuncs.cpp\
-	$(PROCESSING_SRCDIR)/./src/vaacalc/vaacalculation.cpp\
+	$(PROCESSING_SRCDIR)/src/adaptivequantization/AdaptiveQuantization.cpp\
+	$(PROCESSING_SRCDIR)/src/backgrounddetection/BackgroundDetection.cpp\
+	$(PROCESSING_SRCDIR)/src/common/memory.cpp\
+	$(PROCESSING_SRCDIR)/src/common/thread.cpp\
+	$(PROCESSING_SRCDIR)/src/common/WelsFrameWork.cpp\
+	$(PROCESSING_SRCDIR)/src/common/WelsFrameWorkEx.cpp\
+	$(PROCESSING_SRCDIR)/src/complexityanalysis/ComplexityAnalysis.cpp\
+	$(PROCESSING_SRCDIR)/src/denoise/denoise.cpp\
+	$(PROCESSING_SRCDIR)/src/denoise/denoise_filter.cpp\
+	$(PROCESSING_SRCDIR)/src/downsample/downsample.cpp\
+	$(PROCESSING_SRCDIR)/src/downsample/downsamplefuncs.cpp\
+	$(PROCESSING_SRCDIR)/src/imagerotate/imagerotate.cpp\
+	$(PROCESSING_SRCDIR)/src/imagerotate/imagerotatefuncs.cpp\
+	$(PROCESSING_SRCDIR)/src/scenechangedetection/SceneChangeDetection.cpp\
+	$(PROCESSING_SRCDIR)/src/scenechangedetection/SceneChangeDetectionCommon.cpp\
+	$(PROCESSING_SRCDIR)/src/vaacalc/vaacalcfuncs.cpp\
+	$(PROCESSING_SRCDIR)/src/vaacalc/vaacalculation.cpp\
 
 PROCESSING_OBJS += $(PROCESSING_CPP_SRCS:.cpp=.o)
 ifeq ($(USE_ASM), Yes)
 PROCESSING_ASM_SRCS=\
-	$(PROCESSING_SRCDIR)/./src/asm/denoisefilter.asm\
-	$(PROCESSING_SRCDIR)/./src/asm/downsample_bilinear.asm\
-	$(PROCESSING_SRCDIR)/./src/asm/vaa.asm\
+	$(PROCESSING_SRCDIR)/src/asm/denoisefilter.asm\
+	$(PROCESSING_SRCDIR)/src/asm/downsample_bilinear.asm\
+	$(PROCESSING_SRCDIR)/src/asm/vaa.asm\
 
 PROCESSING_OBJS += $(PROCESSING_ASM_SRCS:.asm=.o)
 endif
