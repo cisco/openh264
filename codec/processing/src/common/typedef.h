b468ed3c0b26cd4589bff2dd3d358cf84a1db6d7
Martin Storsjö <martin@martin.st>
2014-01-24 23:59:43 +0200

    Unify the declaration of int8_t within the processing lib with the rest
    int8_t in general should to be defined as signed char, since there
    are actual envrionments where a plain 'char' is unsigned.
    
    This also reduces the differences between the typedef headers of
    the different sub-libraries.

--- codec/processing/src/common/typedef.h
+++ codec/processing/src/common/typedef.h
@@ -60,7 +60,7 @@ WELSVP_NAMESPACE_BEGIN
 
 #if defined(_MSC_VER)
 
-typedef char               int8_t   ;
+typedef signed char        int8_t   ;
 typedef unsigned char      uint8_t  ;
 typedef short              int16_t  ;
 typedef unsigned short     uint16_t ;
