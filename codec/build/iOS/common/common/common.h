672a98347f96cf8acbb4b39d59b319bfb5346e37
Licai Guo <guolicai@gmail.com>
2014-02-21 13:59:43 +0800

    Initial Commit

---
+++ codec/build/iOS/common/common/common.h
@@ -0,0 +1,13 @@
+//
+//  common.h
+//  common
+//
+//  Created by guangx on 14-2-21.
+//  Copyright (c) 2014年 video. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface common : NSObject
+
+@end
---
+++ codec/build/iOS/common/commonTests/commonTests.m
@@ -0,0 +1,34 @@
+//
+//  commonTests.m
+//  commonTests
+//
+//  Created by guangx on 14-2-21.
+//  Copyright (c) 2014年 video. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface commonTests : XCTestCase
+
+@end
+
+@implementation commonTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
