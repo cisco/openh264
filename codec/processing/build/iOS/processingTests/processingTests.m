68ebf0540277e311898793d7d2c1a453f43db8b3
Licai Guo <guolicai@gmail.com>
2014-02-21 14:28:22 +0800

    Add ios build for processing

---
+++ codec/processing/build/iOS/processingTests/processingTests.m
@@ -0,0 +1,34 @@
+//
+//  processingTests.m
+//  processingTests
+//
+//  Created by guangx on 14-2-21.
+//  Copyright (c) 2014年 video. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface processingTests : XCTestCase
+
+@end
+
+@implementation processingTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
