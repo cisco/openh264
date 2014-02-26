ab3609c4c7f0f3648db9c4434293b2ca6de341ed
Martin Storsjö <martin@martin.st>
2014-02-26 14:47:22 +0200

    Don't add x86 assembly specific options on darwin if building for other archs
    This avoids warnings about using the -read_only_relocs suppress
    if building for other architectures than x86.

--- build/platform-darwin.mk
+++ build/platform-darwin.mk
@@ -4,11 +4,13 @@ SHAREDLIBSUFFIX = dylib
 SHARED = -dynamiclib
 CFLAGS += -Werror -fPIC -DMACOS -DMT_ENABLED -MMD -MP
 LDFLAGS += -lpthread
+ifeq ($(ASM_ARCH), x86)
 ASMFLAGS += --prefix _ -DNOPREFIX
 ifeq ($(ENABLE64BIT), Yes)
 ASMFLAGS += -f macho64
 else
 ASMFLAGS += -f macho
 LDFLAGS += -read_only_relocs suppress
+endif
 endif
 
