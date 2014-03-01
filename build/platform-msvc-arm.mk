00efbc2cb589df40eddd71463cabbc3517040fe4
Martin Storsjö <martin@martin.st>
2014-03-01 23:02:48 +0200

    Enable the arm assembly in msvc-arm builds
    This requires using the external gas-preprocessor tool that
    converts the gnu assembler format sources to armasm format at
    build time. This allows keeping the source files in one single
    format but only converting while building.

--- build/platform-msvc-arm.mk
+++ build/platform-msvc-arm.mk
@@ -1,5 +1,7 @@
 include build/platform-msvc-common.mk
+ARCH=arm
+include build/platform-arch.mk
 CFLAGS += -DWINAPI_FAMILY=WINAPI_FAMILY_PHONE_APP -MD -DWIN32
 LDFLAGS +=
-USE_ASM = No
+CCAS = gas-preprocessor.pl -as-type armasm -force-thumb -- armasm
 
