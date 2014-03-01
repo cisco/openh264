6f803e16f53d5b31be15508001e326469824ec2f
Martin Storsjö <martin@martin.st>
2014-03-01 02:03:13 +0200

    Only set x86 ASMFLAGS if building x86 assembly

--- build/platform-freebsd.mk
+++ build/platform-freebsd.mk
@@ -2,10 +2,12 @@ include build/platform-arch.mk
 SHAREDLIBSUFFIX = so
 CFLAGS += -fPIC -DMT_ENABLED
 LDFLAGS += -lpthread
+ifeq ($(ASM_ARCH), x86)
 ASMFLAGS += -DNOPREFIX
 ifeq ($(ENABLE64BIT), Yes)
 ASMFLAGS += -f elf64
 else
 ASMFLAGS += -f elf
+endif
 endif
 
--- build/platform-linux.mk
+++ build/platform-linux.mk
@@ -2,10 +2,12 @@ include build/platform-arch.mk
 SHAREDLIBSUFFIX = so
 CFLAGS += -Werror -fPIC -DLINUX -DMT_ENABLED -MMD -MP
 LDFLAGS += -lpthread
+ifeq ($(ASM_ARCH), x86)
 ASMFLAGS += -DNOPREFIX
 ifeq ($(ENABLE64BIT), Yes)
 ASMFLAGS += -f elf64
 else
 ASMFLAGS += -f elf32
+endif
 endif
 
