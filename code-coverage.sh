63ff6b566cdc4c7b7915c901671c03971f3763ba
Licai Guo <guolicai@gmail.com>
2014-03-13 01:46:09 -0700

    remove trailing white spaces

--- code-coverage.sh
+++ code-coverage.sh
@@ -2,5 +2,5 @@
 lcov -b . -d . -c -o tmp.info
 lcov -e tmp.info \*/codec/\* -o gcov.info
 mkdir -p code-coverage
-genhtml gcov.info -o ./code-coverage 
+genhtml gcov.info -o ./code-coverage
 rm -f tmp.info gcov.info
