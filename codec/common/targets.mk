7b225fad189d5444a1592e67eb38c6e06d11d54d
Licai Guo <guolicai@gmail.com>
2014-03-18 18:16:00 -0700

    reorganize common dir

--- codec/common/targets.mk
+++ codec/common/targets.mk
@@ -1,32 +1,32 @@
 COMMON_SRCDIR=codec/common
 COMMON_CPP_SRCS=\
-	$(COMMON_SRCDIR)/cpu.cpp\
-	$(COMMON_SRCDIR)/crt_util_safe_x.cpp\
-	$(COMMON_SRCDIR)/deblocking_common.cpp\
-	$(COMMON_SRCDIR)/logging.cpp\
-	$(COMMON_SRCDIR)/WelsThreadLib.cpp\
+	$(COMMON_SRCDIR)/src/cpu.cpp\
+	$(COMMON_SRCDIR)/src/crt_util_safe_x.cpp\
+	$(COMMON_SRCDIR)/src/deblocking_common.cpp\
+	$(COMMON_SRCDIR)/src/logging.cpp\
+	$(COMMON_SRCDIR)/src/WelsThreadLib.cpp\
 
 COMMON_OBJS += $(COMMON_CPP_SRCS:.cpp=.$(OBJ))
 
 ifeq ($(ASM_ARCH), x86)
 COMMON_ASM_SRCS=\
-	$(COMMON_SRCDIR)/cpuid.asm\
-	$(COMMON_SRCDIR)/deblock.asm\
-	$(COMMON_SRCDIR)/expand_picture.asm\
-	$(COMMON_SRCDIR)/mb_copy.asm\
-	$(COMMON_SRCDIR)/mc_chroma.asm\
-	$(COMMON_SRCDIR)/mc_luma.asm\
-	$(COMMON_SRCDIR)/satd_sad.asm\
-	$(COMMON_SRCDIR)/vaa.asm\
+	$(COMMON_SRCDIR)/x86/cpuid.asm\
+	$(COMMON_SRCDIR)/x86/deblock.asm\
+	$(COMMON_SRCDIR)/x86/expand_picture.asm\
+	$(COMMON_SRCDIR)/x86/mb_copy.asm\
+	$(COMMON_SRCDIR)/x86/mc_chroma.asm\
+	$(COMMON_SRCDIR)/x86/mc_luma.asm\
+	$(COMMON_SRCDIR)/x86/satd_sad.asm\
+	$(COMMON_SRCDIR)/x86/vaa.asm\
 
 COMMON_OBJS += $(COMMON_ASM_SRCS:.asm=.$(OBJ))
 endif
 
 ifeq ($(ASM_ARCH), arm)
 COMMON_ASM_S_SRCS=\
-	$(COMMON_SRCDIR)/deblocking_neon.S\
-	$(COMMON_SRCDIR)/expand_picture_neon.S\
-	$(COMMON_SRCDIR)/mc_neon.S\
+	$(COMMON_SRCDIR)/arm/deblocking_neon.S\
+	$(COMMON_SRCDIR)/arm/expand_picture_neon.S\
+	$(COMMON_SRCDIR)/arm/mc_neon.S\
 
 COMMON_OBJS += $(COMMON_ASM_S_SRCS:.S=.$(OBJ))
 endif
