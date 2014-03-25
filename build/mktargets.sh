de22cb50031ce28c0ae968eb2d13459f174e3f4c
Licai Guo <guolicai@gmail.com>
2014-03-24 18:21:45 -0700

    reorganize tests

--- build/mktargets.sh
+++ build/mktargets.sh
@@ -6,5 +6,7 @@ python build/mktargets.py --directory codec/processing --library processing
 
 python build/mktargets.py --directory codec/console/dec --binary h264dec
 python build/mktargets.py --directory codec/console/enc --binary h264enc
-python build/mktargets.py --directory test --binary codec_unittest
+python build/mktargets.py --directory test/encoder --prefix encoder_unittest
+python build/mktargets.py --directory test/decoder --prefix decoder_unittest
+python build/mktargets.py --directory test/api --prefix api_test
 python build/mktargets.py --directory gtest --library gtest --out build/gtest-targets.mk --cpp-suffix .cc --include gtest-all.cc
