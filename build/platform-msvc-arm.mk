1ee52616ac92a43abddf1b80549884c65d24f185
Martin Storsjö <martin@martin.st>
2014-04-11 10:47:29 +0300

    Remove a duplicate -MD declaration

--- build/platform-msvc-arm.mk
+++ build/platform-msvc-arm.mk
@@ -3,7 +3,7 @@ CFLAGS_OPT += -MD
 CFLAGS_DEBUG += -MDd
 ARCH=arm
 include build/platform-arch.mk
-CFLAGS += -DWINAPI_FAMILY=WINAPI_FAMILY_PHONE_APP -MD -DWIN32
+CFLAGS += -DWINAPI_FAMILY=WINAPI_FAMILY_PHONE_APP -DWIN32
 CXXFLAGS += -ZW
 LDFLAGS +=
 CCAS = gas-preprocessor.pl -as-type armasm -force-thumb -- armasm
