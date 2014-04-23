f1b2d51d8624431ca161f20149d157117c1c16f6
Martin Storsjö <martin@martin.st>
2014-04-23 10:53:39 +0300

    Add support for building the arm64 assembly in platform-arch.mk

--- build/platform-arch.mk
+++ build/platform-arch.mk
@@ -8,3 +8,10 @@ ASMFLAGS += -Icodec/common/arm/
 CFLAGS += -DHAVE_NEON
 endif
 endif
+ifneq ($(filter arm64 aarch64, $(ARCH)),)
+ifeq ($(USE_ASM), Yes)
+ASM_ARCH = arm64
+ASMFLAGS += -Icodec/common/arm64/
+CFLAGS += -DHAVE_NEON_AARCH64
+endif
+endif
