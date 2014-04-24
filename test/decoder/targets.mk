4a6259cf74617e15a4e0f0b3fdd8a749ed17499e
huili2 <huili2@cisco.com>
2014-04-23 19:55:38 -0700

    add slice/frame copy UT for EC

--- test/decoder/targets.mk
+++ test/decoder/targets.mk
@@ -1,5 +1,6 @@
 DECODER_UNITTEST_SRCDIR=test/decoder
 DECODER_UNITTEST_CPP_SRCS=\
+        $(DECODER_UNITTEST_SRCDIR)/DecUT_ErrorConcealment.cpp\
 	$(DECODER_UNITTEST_SRCDIR)/DecUT_ExpandPicture.cpp\
 	$(DECODER_UNITTEST_SRCDIR)/DecUT_IdctResAddPred.cpp\
 	$(DECODER_UNITTEST_SRCDIR)/DecUT_IntraPrediction.cpp\
