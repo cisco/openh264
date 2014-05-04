9d0647b8ac2a937773d186963cbc8032330d4de1
Martin Storsjö <martin@martin.st>
2014-05-04 15:54:16 +0300

    Use $(PROJECT_NAME) for naming the DLL import library as well

--- build/platform-msvc-common.mk
+++ build/platform-msvc-common.mk
@@ -33,5 +33,5 @@ EXEEXT=.exe
 OBJ=obj
 SHAREDLIBSUFFIX=dll
 SHARED=-LD
-SHLDFLAGS=-link -def:wels.def -implib:wels_dll.lib
-EXTRA_LIBRARY=wels_dll.lib
+SHLDFLAGS=-link -def:wels.def -implib:$(PROJECT_NAME)_dll.lib
+EXTRA_LIBRARY=$(PROJECT_NAME)_dll.lib
