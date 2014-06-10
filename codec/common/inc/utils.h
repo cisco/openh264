90be3d8215c104bedc6027f881abd81c78ad27e2
Martin Storsjö <martin@martin.st>
2014-06-10 11:39:17 +0300

    Don't treat log levels as a bitmask
    All use of log levels in the library just do a numerical
    greater-than comparison between the set log level and the
    level of the current message.

--- codec/common/inc/utils.h
+++ codec/common/inc/utils.h
@@ -55,7 +55,7 @@ enum {
   WELS_LOG_DEBUG		= 1 << 3,	// Debug log iLevel
   WELS_LOG_RESV		= 1 << 4,	// Resversed log iLevel
   WELS_LOG_LEVEL_COUNT = 5,
-  WELS_LOG_DEFAULT	= WELS_LOG_ERROR | WELS_LOG_WARNING | WELS_LOG_INFO | WELS_LOG_DEBUG	// Default log iLevel in Wels codec
+  WELS_LOG_DEFAULT	= WELS_LOG_DEBUG	// Default log iLevel in Wels codec
 };
 
 /*
