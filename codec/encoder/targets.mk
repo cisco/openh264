7fc869cf0f89872967afe5fe15e34d856c8d8e0c
Martin Storsjö <martin@martin.st>
2014-07-08 11:42:05 +0300

    Regenerate the targets.mk files
    This makes the indentation of some new lines consistent with the rest.

--- codec/encoder/targets.mk
+++ codec/encoder/targets.mk
@@ -63,7 +63,7 @@ ENCODER_ASM_ARM64_SRCS=\
 	$(ENCODER_SRCDIR)/core/arm64/intra_pred_sad_3_opt_aarch64_neon.S\
 	$(ENCODER_SRCDIR)/core/arm64/memory_aarch64_neon.S\
 	$(ENCODER_SRCDIR)/core/arm64/pixel_aarch64_neon.S\
-        $(ENCODER_SRCDIR)/core/arm64/reconstruct_aarch64_neon.S\
+	$(ENCODER_SRCDIR)/core/arm64/reconstruct_aarch64_neon.S\
 
 ENCODER_OBJS += $(ENCODER_ASM_ARM64_SRCS:.S=.$(OBJ))
 endif
