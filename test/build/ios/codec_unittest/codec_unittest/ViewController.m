e1840a0ed6e5d093343ec67df0cb286b77c4b1d7
Martin Storsjö <martin@martin.st>
2015-06-03 09:56:30 +0300

    Change indentation to use tabs instead of spaces in a few test project files

--- test/build/ios/codec_unittest/codec_unittest/ViewController.m
+++ test/build/ios/codec_unittest/codec_unittest/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning
