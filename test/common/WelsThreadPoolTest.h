a50c127fa691b247efe82b43a0ff8a870a9b62f9
sijchen <sijchen@cisco.com>
2016-04-04 10:47:29 -0700

    remove unneeded

--- test/common/WelsThreadPoolTest.h
+++ test/common/WelsThreadPoolTest.h
@@ -29,17 +29,17 @@ class CThreadPoolTest : public IWelsTaskSink {
   }*/
 
   virtual int32_t OnTaskExecuted() {
-    fprintf(stdout, "OnTaskExecuted %x\n", this);
+    //fprintf(stdout, "OnTaskExecuted %x\n", this);
     WelsCommon::CWelsAutoLock cAutoLock (m_cTaskCountLock);
     m_iTaskCount ++;
-    fprintf(stdout, "Task execute over count is %d\n", m_iTaskCount);
+    //fprintf(stdout, "Task execute over count is %d\n", m_iTaskCount);
     return cmResultSuccess;
   }
 
   virtual int32_t OnTaskCancelled() {
     WelsCommon::CWelsAutoLock cAutoLock (m_cTaskCountLock);
     m_iTaskCount ++;
-    fprintf(stdout, "Task execute cancelled count is %d\n", m_iTaskCount);
+    //fprintf(stdout, "Task execute cancelled count is %d\n", m_iTaskCount);
     return cmResultSuccess;
   }
 
