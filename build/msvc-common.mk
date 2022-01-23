900c15d00d916c4bcc1198f0e2e90a0ed6b905d2
Martin Storsjö <martin@martin.st>
2022-01-24 00:09:47 +0200

    msvc: Stop unconditionally building with -WX (erroring out on any warning)
    This was added without a reason in
    c256ae3b2774030966913e47a9088ec47d053535.
    
    Options like -Werror or -WX may make sense when built as part of CI,
    to catch warnings in those known warning-free configurations. But
    the build system itself shouldn't enable such errors by default,
    as that can break building for users with different compiler versions.

--- build/msvc-common.mk
+++ build/msvc-common.mk
@@ -35,7 +35,7 @@ endif
 # it unconditionally. The same issue can also be worked around by adding
 # -DGTEST_HAS_TR1_TUPLE=0 instead, but we prefer this version since it
 # matches what gtest itself does.
-CFLAGS += -nologo -W3 -WX -EHsc -fp:precise -Zc:wchar_t -Zc:forScope -D_VARIADIC_MAX=10
+CFLAGS += -nologo -W3 -EHsc -fp:precise -Zc:wchar_t -Zc:forScope -D_VARIADIC_MAX=10
 CXX_LINK_O=-nologo -Fe$@
 AR_OPTS=-nologo -out:$@
 CFLAGS_OPT=-O2 -Ob1 -Oy- -Zi -GF -GS -Gy -DNDEBUG
