6b9e44911aaee87150599ecabac710a666498682
Martin Storsjö <martin@martin.st>
2022-01-24 05:00:13 +0200

    Clean up MSVC build after c256ae3b2774030966913e47a9088ec47d053535 (#3470)

--- build/msvc-common.mk
+++ build/msvc-common.mk
@@ -2,15 +2,9 @@ include $(SRC_PATH)build/arch.mk
 ifeq ($(ASM_ARCH), x86)
 ifeq ($(ARCH), x86_64)
 ASMFLAGS += -f win64
-ASMFLAGS_PLATFORM = -DWIN64 -D_WIN64
-CFLAGS += -D_WIN64
-CXXFLAGS += -D_WIN64
-COMMON_CFLAGS += -D_WIN64
+ASMFLAGS_PLATFORM = -DWIN64
 else
 ASMFLAGS += -f win32 -DPREFIX
-CFLAGS += -DWIN32
-CXXFLAGS += -DWIN32
-COMMON_CFLAGS += -DWIN32
 endif
 else
 endif
@@ -25,14 +19,8 @@ AR=lib
 CXX_O=-Fo$@
 
 ifeq ($(ASM_ARCH), arm64)
-CC = clang-cl
-CXX = clang-cl
 CCAS = clang-cl
-CFLAGS += -DWIN32 -nologo -DHAVE_NEON_AARCH64 --target=arm64-windows
 CCASFLAGS = -nologo -DHAVE_NEON_AARCH64 --target=arm64-windows
-LDFLAGS += -link
-else
-LDFLAGS += -link -cetcompat
 endif
 
 
@@ -41,7 +29,7 @@ endif
 # it unconditionally. The same issue can also be worked around by adding
 # -DGTEST_HAS_TR1_TUPLE=0 instead, but we prefer this version since it
 # matches what gtest itself does.
-CFLAGS += -nologo -W3 -WX -EHsc -fp:precise -Zc:wchar_t -Zc:forScope -D_VARIADIC_MAX=10
+CFLAGS += -nologo -W3 -EHsc -fp:precise -Zc:wchar_t -Zc:forScope -D_VARIADIC_MAX=10
 CXX_LINK_O=-nologo -Fe$@
 AR_OPTS=-nologo -out:$@
 CFLAGS_OPT=-O2 -Ob1 -Oy- -Zi -GF -GS -Gy -DNDEBUG
@@ -60,10 +48,14 @@ SHAREDLIBSUFFIXFULLVER=$(SHAREDLIBSUFFIX)
 SHAREDLIBSUFFIXMAJORVER=$(SHAREDLIBSUFFIX)
 SHARED=-LD
 EXTRA_LIBRARY=$(PROJECT_NAME)_dll.lib
-
+LDFLAGS += -link
 SHLDFLAGS=-debug -map -opt:ref -opt:icf -def:$(SRC_PATH)openh264.def -implib:$(EXTRA_LIBRARY)
 STATIC_LDFLAGS=
 CODEC_UNITTEST_CFLAGS+=-D_CRT_SECURE_NO_WARNINGS
+
+ifneq ($(filter %86 x86_64, $(ARCH)),)
+LDFLAGS += -cetcompat
+endif
 
 %.res: %.rc
 	$(QUIET_RC)rc -fo $@ $<
