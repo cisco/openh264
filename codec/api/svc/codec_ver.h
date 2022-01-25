97e7e6758459af1754e40a0074af317272aad98f
Guangwei Wang <guangwwa@cisco.com>
2022-01-25 12:49:53 +0800

    update version to 2.2.0

--- codec/api/svc/codec_ver.h
+++ codec/api/svc/codec_ver.h
@@ -4,12 +4,12 @@
 
 #include "codec_app_def.h"
 
-static const OpenH264Version g_stCodecVersion  = {2, 1, 1, 2201};
-static const char* const g_strCodecVer  = "OpenH264 version:2.1.1.2201";
+static const OpenH264Version g_stCodecVersion  = {2, 2, 0, 2201};
+static const char* const g_strCodecVer  = "OpenH264 version:2.2.0.2201";
 
 #define OPENH264_MAJOR (2)
-#define OPENH264_MINOR (1)
-#define OPENH264_REVISION (1)
+#define OPENH264_MINOR (2)
+#define OPENH264_REVISION (0)
 #define OPENH264_RESERVED (2201)
 
 #endif  // CODEC_VER_H
