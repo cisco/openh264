a1be4e734019554dc91af5313021068c7df3d3bd
Martin Storsjö <martin@martin.st>
2022-05-31 11:27:20 +0300

    build: Remove specific CC/CXX/AR defaults in platform-mingw_nt.mk
    If building on native mingw, these don't need to be set manually
    (x86_64-w64-mingw32-gcc does exist, but x86_64-w64-mingw32-ar
    doesn't). If cross compiling, it's best to let the caller specify
    them explicitly.
    
    This fixes building in msys2/mingw64.

--- build/platform-mingw_nt.mk
+++ build/platform-mingw_nt.mk
@@ -11,9 +11,6 @@ ifeq ($(ASM_ARCH), x86)
 ifeq ($(ARCH), x86_64)
 ASMFLAGS += -f win64
 ASMFLAGS_PLATFORM = -DWIN64
-CC = x86_64-w64-mingw32-gcc
-CXX = x86_64-w64-mingw32-g++
-AR = x86_64-w64-mingw32-ar
 else
 ASMFLAGS += -f win32 -DPREFIX
 endif
