8fe55e3f4accac7fd1af4262bcd6eff5d008396d
Guangwei Wang <guangwwa@cisco.com>
2022-06-02 17:39:49 +0800

    update openh264 version to v2.3.0

--- codec/api/wels/codec_ver.h
+++ codec/api/wels/codec_ver.h
@@ -4,12 +4,12 @@
 
 #include "codec_app_def.h"
 
-static const OpenH264Version g_stCodecVersion  = {2, 2, 0, 2201};
-static const char* const g_strCodecVer  = "OpenH264 version:2.2.0.2201";
+static const OpenH264Version g_stCodecVersion  = {2, 3, 0, 2206};
+static const char* const g_strCodecVer  = "OpenH264 version:2.3.0.2206";
 
 #define OPENH264_MAJOR (2)
-#define OPENH264_MINOR (2)
+#define OPENH264_MINOR (3)
 #define OPENH264_REVISION (0)
-#define OPENH264_RESERVED (2201)
+#define OPENH264_RESERVED (2206)
 
 #endif  // CODEC_VER_H
