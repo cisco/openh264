f60e7d9bdc39e51b644db7624256116202cac992
matoro <matoro@users.noreply.github.com>
2023-03-02 17:39:45 -0500

    Use environment for mips feature detection
    The -march= option is perfectly happy to emit code to run on a processor
    different than the one on which it is being compiled.  This results in
    misdetection of mips features because the test compiles specify that a
    given extension should be emitted, but this does not check whether or
    not this corresponds to the subarchitecture targeted in CFLAGS by the
    rest of the build.
    
    $ echo "void main(void){ __asm__ volatile(\"punpcklhw \$f0, \$f0, \$f0\"); }" > test.c
    $ CFLAGS="-march=loongson3a" make test
    cc -march=loongson3a    test.c   -o test
    $ ./test
    Illegal instruction
    $ CFLAGS="-march=native" make -B test
    cc -march=native    test.c   -o test
    /tmp/ccLbeyM1.s: Assembler messages:
    /tmp/ccLbeyM1.s:25: Error: opcode not supported on this processor: octeon2 (mips64r2) `punpcklhw $f0,$f0,$f0'
    make: *** [<builtin>: test] Error 1
    
    This leads to -march=loongson3a getting appended to CFLAGS, which may
    conflict with previously specified -march= levels for the build, or
    other options.  Calling make in the test will use whatever CC/CFLAGS are
    specified in the environment to determine whether the actual compile
    command line to be used in the build supports these features.
    
    Fixes: 8b942ee ("Adjust the mmi/msa detection mode for mips platform.")

--- build/loongarch-simd-check.sh
+++ build/loongarch-simd-check.sh
@@ -8,29 +8,26 @@
 #    lsx, lasx (maybe more in the future).
 #
 #   --usage:
-#             ./loongarch-simd-check.sh $(CC) lsx
-#         or  ./loongarch-simd-check.sh $(CC) lasx
+#             ./loongarch-simd-check.sh lsx
+#         or  ./loongarch-simd-check.sh lasx
 #
 # date:  11/23/2021 Created
 #***************************************************************************************
 
 TMPC=$(mktemp tmp.XXXXXX.c)
-TMPO=$(mktemp tmp.XXXXXX.o)
-if [ $2 == "lsx" ]
+if [ $1 == "lsx" ]
 then
    echo "void main(void){ __asm__ volatile(\"vadd.b \$vr0, \$vr1, \$vr1\"); }" > $TMPC
-   $1 -mlsx $TMPC -o $TMPO &> /dev/null
-   if test -s $TMPO
+   if make -f /dev/null "${TMPC/.c/.o}"
    then
       echo "Yes"
    fi
-elif [ $2 == "lasx" ]
+elif [ $1 == "lasx" ]
 then
    echo "void main(void){ __asm__ volatile(\"xvadd.b \$xr0, \$xr1, \$xr1\"); }" > $TMPC
-   $1 -mlasx $TMPC -o $TMPO &> /dev/null
-   if test -s $TMPO
+   if make -f /dev/null "${TMPC/.c/.o}"
    then
       echo "Yes"
    fi
 fi
-rm -f $TMPC $TMPO
+rm -f $TMPC
