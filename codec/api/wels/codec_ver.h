34e14eab6863154f75a94e5dca97cb4324f98bac
BenzhengZhang <140143892+BenzhengZhang@users.noreply.github.com>
2023-10-27 02:35:18 -0500

    update openh264 version to v2.4.0 (#3691)

--- codec/api/wels/codec_ver.h
+++ codec/api/wels/codec_ver.h
@@ -4,12 +4,12 @@
 
 #include "codec_app_def.h"
 
-static const OpenH264Version g_stCodecVersion  = {2, 3, 0, 2206};
-static const char* const g_strCodecVer  = "OpenH264 version:2.3.0.2206";
+static const OpenH264Version g_stCodecVersion  = {2, 4, 0, 2310};
+static const char* const g_strCodecVer  = "OpenH264 version:2.4.0.2310";
 
 #define OPENH264_MAJOR (2)
-#define OPENH264_MINOR (3)
+#define OPENH264_MINOR (4)
 #define OPENH264_REVISION (0)
-#define OPENH264_RESERVED (2206)
+#define OPENH264_RESERVED (2310)
 
 #endif  // CODEC_VER_H
