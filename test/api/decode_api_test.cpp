478e5ab3eca30e600006d5a0a08b176fd34d3bd1
Philipp Hancke <phancke@meta.com>
2024-07-02 19:45:04 -0700

    Change picOrderCnt to 2 (#3752)
    * Change picOrderCnt to 2
    
    changes the picOrderCnt field to 2 (which means the output order
    is the same as the decode order) which is more suited to realtime
    usage and allows the decoder to immediately decode a frame without
    waiting for out-of-order frames. This reduces the decoding latency
    on Qualcomm hardware decoders which do not honor the VUI parameter
      max_num_reorder_frames
    
    * bump hashes
    
    * bump asan hashes
    
    * Revert "bump asan hashes"
    
    This reverts commit 7aa8b3d747b498a395d78bb9763d9b770fd5910b.
    
    * fix last hashes
    
    * update second hash for ASAN
    
    * fix more hashes

--- test/api/decode_api_test.cpp
+++ test/api/decode_api_test.cpp
@@ -781,9 +781,9 @@ const uint32_t kiFrameNum = 100; //DO NOT CHANGE!
 const char* const pHashStr[][2] = { //DO NOT CHANGE!
   // Allow for different output depending on whether averaging is done
   // vertically or horizontally first when downsampling.
-  { "d5fb6d72f8cc0ea4b037e883598c162fd32b475d", "0fc7e06d0d766ac911730da2aa9e953bc858a161" },
-  { "17203f07486e895aef7c1bf94133fd731caba572", "1d47de674c9c44d8292ee00fa053a42bb9383614" },
-  { "86bf890aef2abe24abe40ebe3d9ec76a25ddebe7", "43eaac708413c109ca120c5d570176f1c9b4036c" }
+  { "c42f9f9bd9084a7fe1dc2e1788cdb1dc3a1cbc61", "8c1a1f2d1c01ac1f9da2f883a55191965691d199" },
+  { "93b2df27e94464f355b60343c786105075fc96d8", "a5634698fef84d402218806f48c6b48e79d4ec4d" },
+  { "8e2d7cd8de6dfb03189a3ac13c289393600780a2", "c9904fdfed2108414597af96a2561680d9a17acd" }
 };
 
 class DecodeParseAPI : public ::testing::TestWithParam<EncodeDecodeFileParamBase>, public EncodeDecodeTestBase {
