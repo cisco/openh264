cisco/openh264

commit 1c2388750acf8646ca75a6038a544d5f8f47dca8

Fix race issue among decoding threads which causes broken frame. (#3735)
Trim event timings for multi-thread cooperation.

codec/decoder/plus/src/welsDecoderExt.cpp
@@ -695,9 +695,11 @@ DECODING_STATE CWelsDecoder::DecodeFrameNoDelay (const unsigned char* kpSrc,
     SBufferInfo* pDstInfo) {
   int iRet = dsErrorFree;
   if (m_iThreadCount >= 1) {
+    SET_EVENT (&m_sReleaseBufferEvent);
     iRet = ThreadDecodeFrameInternal (kpSrc, kiSrcLen, ppDst, pDstInfo);
     if (m_sReoderingStatus.iNumOfPicts) {
       WAIT_EVENT (&m_sBufferingEvent, WELS_DEC_THREAD_WAIT_INFINITE);
+      RESET_EVENT (&m_sBufferingEvent);
       RESET_EVENT (&m_sReleaseBufferEvent);
       if (!m_sReoderingStatus.bHasBSlice) {
         if (m_sReoderingStatus.iNumOfPicts > 1) {
@@ -707,7 +709,6 @@ DECODING_STATE CWelsDecoder::DecodeFrameNoDelay (const unsigned char* kpSrc,
       else {
         ReleaseBufferedReadyPictureReorder (NULL, ppDst, pDstInfo);
       }
-      SET_EVENT(&m_sReleaseBufferEvent);
     }
     return (DECODING_STATE)iRet;
   }
@@ -793,6 +794,10 @@ DECODING_STATE CWelsDecoder::DecodeFrame2WithCtx (PWelsDecoderContext pDecContex
   pDecContext->iFrameNum = -1; //initialize
 #endif
 
+  if (GetThreadCount (pDecContext) >= 1) {
+    WAIT_EVENT (&m_sReleaseBufferEvent, WELS_DEC_THREAD_WAIT_INFINITE);
+  }
+
   pDecContext->iFeedbackTidInAu = -1; //initialize
   pDecContext->iFeedbackNalRefIdc = -1; //initialize
   if (pDstInfo) {
@@ -875,8 +880,6 @@ DECODING_STATE CWelsDecoder::DecodeFrame2WithCtx (PWelsDecoderContext pDecContex
 
     OutputStatisticsLog (*pDecContext->pDecoderStatistics);
     if (GetThreadCount (pDecContext) >= 1) {
-      WAIT_EVENT (&m_sReleaseBufferEvent, WELS_DEC_THREAD_WAIT_INFINITE);
-      RESET_EVENT (&m_sBufferingEvent);
       BufferingReadyPicture (pDecContext, ppDst, pDstInfo);
       SET_EVENT (&m_sBufferingEvent);
     } else {
@@ -901,8 +904,6 @@ DECODING_STATE CWelsDecoder::DecodeFrame2WithCtx (PWelsDecoderContext pDecContex
   pDecContext->dDecTime += (iEnd - iStart) / 1e3;
 
   if (GetThreadCount (pDecContext) >= 1) {
-    WAIT_EVENT (&m_sReleaseBufferEvent, WELS_DEC_THREAD_WAIT_INFINITE);
-    RESET_EVENT (&m_sBufferingEvent);
     BufferingReadyPicture (pDecContext, ppDst, pDstInfo);
     SET_EVENT (&m_sBufferingEvent);
   } else {

commit a15af5e05f5ca23ed6fb97cd8735af4904fab571

Fix deadlock at the end of decoding.
For some h264 files, h264dec hangs at the end of decoding. This is
regression caused by the commit 1c2388750acf. This patch fixes the
issue by adding SET_EVENT(&m_sReleaseBufferEvent) to SetOption(
DECODER_OPTION_END_OF_STREAM) and CloseDecoderThreads().

codec/decoder/plus/src/welsDecoderExt.cpp
@@ -325,6 +325,7 @@ void CWelsDecoder::OpenDecoderThreads() {
 }
 void CWelsDecoder::CloseDecoderThreads() {
   if (m_iThreadCount >= 1) {
+    SET_EVENT (&m_sReleaseBufferEvent);
     for (int32_t i = 0; i < m_iThreadCount; i++) { //waiting the completion begun slices
       WAIT_SEMAPHORE (&m_pDecThrCtx[i].sThreadInfo.sIsIdle, WELS_DEC_THREAD_WAIT_INFINITE);
       m_pDecThrCtx[i].sThreadInfo.uiCommand = WELS_DEC_THREAD_COMMAND_ABORT;
@@ -513,6 +514,8 @@ long CWelsDecoder::SetOption (DECODER_OPTION eOptID, void* pOption) {
       if (pDecContext == NULL) return dsInitialOptExpected;
 
       pDecContext->bEndOfStreamFlag = iVal ? true : false;
+      if (iVal && m_iThreadCount >= 1)
+        SET_EVENT (&m_sReleaseBufferEvent);
 
       return cmResultSuccess;
     } else if (eOptID == DECODER_OPTION_ERROR_CON_IDC) { // Indicate error concealment status