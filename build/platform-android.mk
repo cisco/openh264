9a7cd6cdaa3968fe700c451d25855590dd8b380f
BENZHENG ZHANG <csuzbz@foxmail.com>
2026-09-20 16:43:09 +0800

    Align remaining Android libraries to 16 KB pages

--- build/platform-android.mk
+++ build/platform-android.mk
@@ -46,7 +46,7 @@ endif
 CFLAGS += -DANDROID_NDK -fpic -MMD -MP -fstack-protector-all
 CXXFLAGS += -fno-rtti -fno-exceptions
 SHLDFLAGS = -Wl,--no-undefined -Wl,-z,relro -Wl,-z,now -Wl,-z,max-page-size=16384 -Wl,-z,common-page-size=16384 -Wl,-soname,lib$(PROJECT_NAME).so
-UTSHLDFLAGS = -Wl,-soname,libut.so
+UTSHLDFLAGS = -Wl,-z,max-page-size=16384 -Wl,-z,common-page-size=16384 -Wl,-soname,libut.so
 
 ifeq ($(NDK_TOOLCHAIN_VERSION), clang)
   LLVM_INSTALL_DIR = $(wildcard $(NDKROOT)/toolchains/llvm/prebuilt/*/bin)
